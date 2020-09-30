c65404350d4687a1a77215d733cc2e43e0d77a37
Prabhanshu-dev <63950638+Prabhanshu-dev@users.noreply.github.com>
2020-09-30 22:45:48 +0530

    Add files via upload

---
+++ Q9.java
@@ -0,0 +1,16 @@
+package Assignment2;
+class Q9
+{
+  public static void main(String[]args)
+ {
+  int j;
+   for(int i=0;i<=5;i++)
+   {
+     for(j=6-i;j>=1;j--)
+     {
+      System.out.print("*");
+     }
+
+    System.out.println();
+   }
+}}
