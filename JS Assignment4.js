a29f06fb9984b6cdabc4d052bf73c253e45381a2
Prabhanshu-dev <63950638+Prabhanshu-dev@users.noreply.github.com>
2021-01-25 19:10:10 +0530

    Add files via upload

--- JS Assignment4.js
+++ JS Assignment4.js
@@ -1,78 +1,26 @@
-1. <body>
-   FirstName:<input type="text"name="u_field"><small id="u_small"></small>
-   <script>
-   function validateResume()
-   {
-    var ptr=/^([0-9\w]*&/;
-   if(ptr.test(document.resume.u_field.value))
-   {
-     document.getElementById("Small")
-      textContext="User is valid"
-      document.getElementById("u_small").style.color="green";
-   }
-    else
-    {
-      document.getElementById("u_small")textContent="user is valid";
-     document.getElementById("u_small")style.color="read";
-    }
-  }
+1.
 
-3.<script>  
-function GFGFun() {  
-    var str = "Visit geeksforGeeks";  
-    var n = str.search(/GeeksforGeeks/i);  
-    document.write(n);  
-}  
-GFGFun();  
-</script>  
 
-<script> 
-function myFunction() { 
-  
-    // input string 
-    var str = "Visit geeksforGeeks!";  
-  
-    // searching string with modifier i 
-    var n = str.search(/GeeksforGeeks/i);  
-  
-    document.write(n + '<br>'); 
-  
-    // searching string without modifier i 
-    var n = str.search(/GeeksforGeeks/); 
-  
-    document.write(n); 
-}  
-myFunction(); 
-</script> 
+2.
 
-5.<!DOCTYPE html>
-<html>
-<head>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script>
-$(document).ready(function(){
-  $("button").click(function(){
-    $("#w3s").attr("href", "https://www.w3schools.com/jquery/");
-  });
+3.var str = "Is this all there is?";
+var patt1 = /[^h]/g;
+
+4.var listItems = $( 'li' );
+var rawListItem = listItems[1]; // or listItems.get(0)
+var html = rawListItem.innerHTML;
+console.log(html);
+
+
+5.$("a").attr("href", "www.w3resource.com/mysql/mysql-tutorials.php");
+
+$("a").text("MySQL Tutorial");
+
+6. $(document).ready(function() { 
+                    $("div").click(function() { 
+                        $("div").dblclick(); 
+                    }); 
+
+7.$("h1,h2,h3" ).click(function() {
+  $( this ).slideUp();
 });
-</script>
-</head>
-<body>
-
-<p><a href="https://www.w3schools.com" id="w3s">W3Schools.com</a></p>
-
-<button>Change href Value</button>
-
-<p>Mouse over the link (or click on it) to see that the value of the href attribute has changed.</p>
-
-</body>
-</html>
-
-6.$("p").bind("click", function(){
-   $( "<p>This is a click Event</p>").appendTo( "#result" );
-   });
-   $("p").bind("dblclick", function(){
-     $( "<p>This is a double-click Event</p>"  ).appendTo( "#result" );
-	 });
-	 
-7.$(document). ready(function(){ $(body). css({ 'cursor' : 'none' }); });	 
